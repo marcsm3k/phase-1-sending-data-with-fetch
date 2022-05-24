6f5c850e17bd7dcf3adb9a56892b5de6d8b33c1f
Marco <marcovelardejr@gmail.com>
2022-05-23 19:37:07 -0500

    commit tree ad93568c6f1e5826bba0f0c49b6b23a17dab81b5

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData (firstName, email) {
+    const userObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", userObj)
+    .then(res => res.json())
+    .then(body => addNewUser(body.id))
+
+    .catch(response => document.body.append('Unauthorized Access'));
+}
+    function addNewUser(newID){
+        document.getElementsByTagName("body")[0].innerHTML = newID;
+        document.body.append(message)
+    }
+    submitData("Marco", "marco_velarde@hotmail.com"); 
